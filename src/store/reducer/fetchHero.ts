ddbfb1f26c000f99a9d5ca89cc705f8e66a014fe
Yuri Valença <yvc@cin.ufpe.br>
2023-07-02 22:57:08 -0300

    :hammer: Add initial hero, so that the page don't start with nothing
    Signed-off-by: Yuri Valença <yvc@cin.ufpe.br>

--- src/store/reducer/fetchHero.ts
+++ src/store/reducer/fetchHero.ts
@@ -12,7 +12,19 @@ interface HeroState {
 const initialState: HeroState = {
   isLoading: false,
   error: null,
-  hero: emptyHeroObject,
+  hero: {
+    id: 1009368,
+    description: "Wounded, captured and forced to build a weapon by his enemies, billionaire industrialist Tony Stark instead created an advanced suit of armor to save his life and escape captivity. Now with a new outlook on life, Tony uses his money and intelligence to make the world a safer, better place as Iron Man.",
+    name: "Iron Man",
+    thumbnail: {
+      path: "http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55",
+      extension: "jpg",
+    },
+    comics: 2681,
+    series: 688,
+    stories: 4004,
+    events: 31,
+  },
 };
 
 const heroSlice = createSlice({
